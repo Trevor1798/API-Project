65de3a561abf74256a3a5dce837b5e545a47f5bc
Trevor <Trevor1798@yahoo.com>
2022-07-27 17:38:56 -0400

    apiRouter

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const apiRouter = require('./api')
 
-    router.get('/hello/world', function(req, res) {
-        res.cookie('XSRF-TOKEN', req.csrfToken())
-        res.send('Hello World')
+   router.use('./api', apiRouter)
+
+ router.get('/api/csrf/restore', (req, res) => {
+    const csrfToken = req.csrfToken()
+    res.cookie("XSRF-TOKEN", csrfToken)
+    res.status(200)
+    res.json({
+        'XSRF-TOKEN': csrfToken
     })
+ })
 
 
 
