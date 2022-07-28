112ab95ce14c0edd1bf7706a248317ec9bdb53d0
Trevor <Trevor1798@yahoo.com>
2022-07-28 13:37:14 -0400

    changes to placement of router

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -5,10 +5,12 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 
 const { enviroment } = require('./config')
 const isProduction = enviroment === 'production';
+const routes = require('./routes')
 
 const app = express()
 
@@ -24,24 +26,46 @@ app.use(
     helmet.crossOriginResourcePolicy({
         policy: "cross-origin"
     })
-)
-app.use(
-    csurf({
-        cookie: {
-            secure: isProduction,
-            sameSite: isProduction && "Lax",
-            httpOnly: true
-        }
+    )
+    app.use(
+        csurf({
+            cookie: {
+                secure: isProduction,
+                sameSite: isProduction && "Lax",
+                httpOnly: true
+            }
+        })
+        )
+
+        app.use(routes)
+
+
+        app.use((_req, _res, next) => {
+            const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message)
+        err.title = 'Validation error'
+    }
+        next(err)
+  })
+
+  app.use((err, _req, res, next) => {
+    res.status(err.status || 500);
+    console.error(err)
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
     })
-)
-
-const routes = require('./routes')
-app.use(routes)
-
-
-
-
-
+  })
 
 
 
