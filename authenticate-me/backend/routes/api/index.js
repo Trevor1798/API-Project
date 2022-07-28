a4f99b1479760a441058274ca52c5867d7b61b8a
Trevor <Trevor1798@yahoo.com>
2022-07-28 17:34:07 -0400

    Add User authentication Express middleware-fixed

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,29 +1,38 @@
+const { restoreUser } = require('../../utils/auth.js');
 const router = require('express').Router()
 const { User } = require('../../db/models');
 const { setTokenCookie } = require('../../utils/auth.js');
-const { restoreUser } = require('../../utils/auth.js');
+
 
 router.use(restoreUser);
-
-
+const { requireAuth } = require('../../utils/auth.js');
 router.get(
-  '/restore-user',
+  '/require-auth',
+  requireAuth,
   (req, res) => {
     return res.json(req.user);
   }
 );
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      }
+
+
+router.get(
+    '/restore-user',
+    (req, res) => {
+        return res.json(req.user);
+    }
+    );
+    router.get('/set-token-cookie', async (req, res) => {
+        const user = await User.findOne({
+            where: {
+                username: 'Demo-lition'
+            }
+        });
+        setTokenCookie(res, user);
+        return res.json({ user });
     });
-  setTokenCookie(res, user);
-  return res.json({ user });
-});
-// router.post('/test', (req, res) => {
-//     res.json({ requestBody: req.body})
-// })
 
+    // router.post('/test', (req, res) => {
+    //     res.json({ requestBody: req.body})
+    // })
 
-module.exports = router;
+    module.exports = router;
--- authenticate-me/backend/utils/auth.js
+++ authenticate-me/backend/utils/auth.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken')
 const {jwtConfig} = require('../config')
 const {User} = require('../db/models')
-
 const {secret, expiresIn} = jwtConfig
+
+
 const setTokenCookie = (res, user) => {
 
     const token = jwt.sign(
