7ed37178788c5ecbffe06f94f1ea0842436005b4
Trevor <Trevor1798@yahoo.com>
2022-07-28 15:36:50 -0400

    Added user authentication Express middleware

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,8 +1,29 @@
 const router = require('express').Router()
+const { User } = require('../../db/models');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { restoreUser } = require('../../utils/auth.js');
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body})
-})
+router.use(restoreUser);
+
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+// router.post('/test', (req, res) => {
+//     res.json({ requestBody: req.body})
+// })
 
 
 module.exports = router;
