6fd360730508fd6846ee0c406b1b058ce856e49b
Trevor <Trevor1798@yahoo.com>
2022-08-03 13:46:00 -0400

    added edit a spot and delete a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -10,6 +10,49 @@ const Sequelize = require('sequelize');
 
 
 
+//Get details of a spot from an id
+router.get('/:spotId', async (req, res) => {
+    const spotId = req.params.ownerId
+    let spots = await Spot.findByPk(spotId)
+
+    if (!spots) {
+        res.status(404)
+        return res.json({
+            "message": "Spot does not exist"
+        })
+    }
+})
+//get spots owned by current user
+router.get('/current-user', restoreUser, requireAuth, async (req, res) => {
+    const currentUser = req.user
+
+    let spotsCurrentlyOwned = await Spot.findAll({
+        where: {
+            currentUser
+        }
+    })
+    return res.json(spotsCurrentlyOwned)
+})
+
+//Add image to spot based on the spots id
+
+router.post('/:spotId/images', restoreUser, requireAuth, async( req, res) => {
+    const spotId = req.params.spotId
+    const currentUser = req.user.id
+    let spot = await Spot.findByPk(spotId)
+
+    if (spot.ownerId !== currentUser) {
+        res.status(404)
+        return res.json({
+            "message": "Spot couldnt be found"
+        })
+    }
+        img = req.body
+        img.spotId = spotId
+
+    const image = await Image.create(img)
+    return res.json(image)
+})
 //Get all Spots
 router.get('/', async (req, res) => {
     const allSpots = await Spot.findAll({
@@ -30,31 +73,8 @@ router.get('/', async (req, res) => {
 
 
 
-//Get details of a spot from an id
-router.get('/:spotId', async (req, res) => {
-    const spotId = req.params.spotId
-    let spots = await Spot.findByPk(spotId)
-
-    if (!spots) {
-        res.status(404)
-        return res.json({
-            "message": "Spot does not exist"
-        })
-    }
-})
 
 
-//get spots owned by current user
-router.get('/current-user', restoreUser, requireAuth, async (req, res) => {
-    const currentUser = req.user
-
-    let spotsCurrentlyOwned = await Spot.findAll({
-        where: {
-            currentUser
-        }
-    })
-    return res.json(spotsCurrentlyOwned)
-})
 
 //Create a spot
 router.post('/', restoreUser, requireAuth, async (req, res) => {
@@ -77,26 +97,33 @@ router.post('/', restoreUser, requireAuth, async (req, res) => {
     return res.json(newSpot)
 })
 
-//Add image to spot based on the spots id
+//edit a spot
 
-router.post('/:spotId/images', restoreUser, requireAuth, async( req, res) => {
-    const spotId = req.params.spotId
-    const currentUser = req.user.id
-    let spot = await Spot.findByPk(spotId)
+router.put('/:spotId', restoreUser, requireAuth, async (req, res) => {
+    let spotId = req.params.spotId
+    let currentUser = req.user.id
 
-    if (spot.ownerId !== currentUser) {
-        res.status(404)
-        return res.json({
-            "message": "Spot couldnt be found"
-        })
-    }
-        img = req.body
-        img.spotId = spotId
+      let spot = await Spot.findByPk(spotId)
+      if (!spot) return res.json({"message": "Spot couldn't be found", "statusCode": 404})
+        if (spot.ownerId === currentUser) {
+         const {address, city, state, country, lat, lng, name, description, price} = req.body
+          spot.update({ address, city, state, country, lat, lng, name, description, price})
+            res.status(200)
+            return res.json(spot)
+        } else return res.json({"message": "Authorization required"})
 
-    const image = await Image.create(img)
-    return res.json(image)
+    })
+
+    //delete a spot
+router.delete('/:spotId', restoreUser, requireAuth, async (req, res) => {
+        const spotId = req.params.spotId
+        const currentUser = req.user.id
+
+            let spot = await Spot.findByPk(spotId)
+            if (!spot) return res.json({"message": "Spot couldn't be found", "statusCode": 404})
+            if (spot.ownerId !== currentUser) return res.json({"message": "Authorization required", "statusCode": 400})
+              await Spot.destroy({where: {id: spotId}})
+                return res.json({"message": "Successfully Deleted"})
 })
-
-
 
 module.exports = router
