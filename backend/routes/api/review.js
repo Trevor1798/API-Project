82b1fcae3abd6da8d0b2a1cd45e0787d01b86336
Trevor <Trevor1798@yahoo.com>
2022-08-03 14:47:51 -0400

    completed route add an image to a review based on the reviews id

--- backend/routes/api/review.js
+++ backend/routes/api/review.js
@@ -30,3 +30,42 @@ router.get('/current-user', restoreUser, requireAuth, async (req, res) => {
 
 
 //add an image to a review based on the reviews ID
+
+router.post('/:reviewId/images', restoreUser, requireAuth, async (req, res) => {
+        const reviewId = req.params.reviewId
+        const currentUser = req.user.id
+        const { url } = req.body
+
+        const newReviewId = await Review.findByPk(reviewId)
+
+        if (!newReviewId) {
+            res.status(404)
+            return res.json({"message": "Review couldnt be found"})
+        }
+        if (!url) {
+           res.status(404)
+            return res.json({"message": "Image url couldn't be found"})
+        }
+
+        const imgNum = await Image.findAll({
+            where: {reviewId: reviewId}
+        })
+        if (imgNum.length > 10) {
+            res.status(403)
+            return res.json({"message": "Maximum number of images for this resource was reached"})
+        }
+
+        const newReviewImage = await Image.create({
+            url,
+            currentUser,
+
+        })
+        res.status(200)
+        return res.json(newReviewImage)
+
+})
+
+
+//edit a review
+
+    
