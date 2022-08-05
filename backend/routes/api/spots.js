a250b6f6f8c77ed168917e5f9e17251f88e3976f
Trevor <Trevor1798@yahoo.com>
2022-08-05 14:31:23 -0400

    got post img working locally

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -114,7 +114,8 @@ router.post('/:spotId/images', restoreUser, requireAuth, async( req, res) => {
             spotId: req.params.spotId,
             userId: userId,
             url,
-            
+            previewImage: previewImage
+
         })
 
         res.status(200)
