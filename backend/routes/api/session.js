86d4e2ff2ac544baf1ca41ab8a1f0811cf6ef87b
Trevor <Trevor1798@yahoo.com>
2022-08-05 21:14:32 -0400

    removed allownull:false validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -26,7 +26,7 @@ router.post(
     async (req, res, next) => {
       const { credential, password } = req.body;
 
-      const user = await User.login({ credential, password });
+      let user = await User.login({ credential, password });
 
       if (!user) {
         const err = new Error('Login failed');
@@ -37,10 +37,9 @@ router.post(
       }
 
      let token = await setTokenCookie(res, user);
+     user = user.toJSON()
       user.token = token
-      return res.json({
-        user
-      });
+      return res.json(user);
     }
   );
 
