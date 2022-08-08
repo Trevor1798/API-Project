a3e32e1d89691eb4cadda66bb3065413f8516bf8
Trevor <Trevor1798@yahoo.com>
2022-08-08 01:48:31 -0400

    done for now

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,42 @@
-const express = require('express'); // handling async route handlers
-require('express-async-errors'); //handling async route handlers
-const morgan = require('morgan'); // logging information about server requests/responses
-const cors = require('cors');
-const csurf = require('csurf'); // CSRF protection
-const helmet = require('helmet'); //security middleware
-const cookieParser = require('cookie-parser'); //parsing cookies from requests
+const express = require("express"); // handling async route handlers
+require("express-async-errors"); //handling async route handlers
+const morgan = require("morgan"); // logging information about server requests/responses
+const cors = require("cors");
+const csurf = require("csurf"); // CSRF protection
+const helmet = require("helmet"); //security middleware
+const cookieParser = require("cookie-parser"); //parsing cookies from requests
 
-const { environment } = require('./config');
-const isProduction = environment === 'production';
-const { ValidationError } = require('sequelize');
+const { environment } = require("./config");
+const isProduction = environment === "production";
+const { ValidationError } = require("sequelize");
 const app = express();
 
 //test...
-const routes = require('./routes');
-app.use(morgan('dev'));
+const routes = require("./routes");
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 
-
-
-
-if (!isProduction){
+if (!isProduction) {
   app.use(cors());
 }
 
 app.use(
   helmet.crossOriginResourcePolicy({
-    policy: "cross-origin"
+    policy: "cross-origin",
   })
-  );
+);
 
-
-
-
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-    );
-    app.use(routes);
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+app.use(routes);
 
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
@@ -52,12 +46,11 @@ app.use((_req, _res, next) => {
   next(err);
 });
 
-
 app.use((err, _req, _res, next) => {
   // check if error is a Sequelize error:
   if (err instanceof ValidationError) {
     err.errors = err.errors.map((e) => e.message);
-    err.title = 'Validation error';
+    err.title = "Validation error";
   }
   next(err);
 });
@@ -66,13 +59,11 @@ app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
   res.json({
-    title: err.title || 'Server Error',
+    title: err.title || "Server Error",
     message: err.message,
     errors: err.errors,
-    stack: isProduction ? null : err.stack
+    stack: isProduction ? null : err.stack,
   });
 });
 
-
-
-module.exports = app
+module.exports = app;
