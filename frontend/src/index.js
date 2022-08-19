d90d3d8de78c1690b46e7ebdcdf1211fdbfd027f
Trevor <Trevor1798@yahoo.com>
2022-08-19 15:14:03 -0400

    caught up to previous work/adding signuppage component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
@@ -13,6 +15,7 @@ if (process.env.NODE_ENV !== "production") {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions
 }
 
 function Root() {
