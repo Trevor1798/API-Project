feaec16a01dc60b5e13c5fb0079c9d3febd61e9a
Trevor <Trevor1798@yahoo.com>
2022-08-23 19:22:20 -0400

    finished filling out basic form for create spot, moving on to handle submit

--- frontend/src/components/Spots/CreateSpot.js
+++ frontend/src/components/Spots/CreateSpot.js
@@ -1,2 +1,124 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+
+
+function CreateSpot () {
+    let dispatch = useDispatch()
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [price, setPrice] = useState()
+    const [address, setAddress] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('')
+    const [country, setCountry] = useState('')
+    const [lat, setLat] = useState('')
+    const [lng, setLng] = useState('')
+    const [previewImage, setPreviewImage] = useState('')
+    const [error, setError] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+    }
+     return (
+        <div className="form-container">
+            <form className="create-spot" onSubmit={handleSubmit}>
+             <ul>{error.map((error, i) => (
+                <li key={i}>{error}</li>
+             ))}
+            </ul>
+            <div className='host-place'>Become a host!</div>
+            <label>
+                <input className="host-name"
+                    type='text'
+                    value={name}
+                    placeholder='Name'
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    />
+            </label>
+            <label>
+                <input className="preview-image"
+                type='text'
+                placeholder='Image URL'
+                value={previewImage}
+                onChange={(e) => setPreviewImage(e.target.value)}
+                required
+                />
+            </label>
+            <label>
+                <input className="host-address"
+                    type='text'
+                    placeholder='Address'
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                    />
+            </label>
+            <label>
+                <input className='host-city'
+                    type='text'
+                    placeholder='City'
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    required
+                    />
+            </label>
+            <label>
+                <input className='host-state'
+                type='text'
+                placeholder="State"
+                value={state}
+                onChange={(e) => setState(e.target.value)}
+                required
+                />
+            </label>
+            <label>
+                <input className="host-country"
+                    type='text'
+                    placeholder='Country'
+                    value={country}
+                    onChange={(e) => setCountry(e.target.value)}
+                    required
+                    />
+            </label>
+            <label>
+                <input className='host-lat'
+                    type='text'
+                    placeholder="Latitude"
+                    value={lat}
+                    onChange={(e) => setLat(e.target.value)}
+                    />
+            </label>
+            <label>
+                <input className='host-lng'
+                    type='text'
+                    placeholder="Longitude"
+                    value={lng}
+                    onChange={(e) => setLng(e.target.value)}
+                    />
+            </label>
+            <label>
+                <input className='host-description'
+                    type='text'
+                    placeholder="Describe your spot"
+                    value={description}
+                    onChange={(e) =>  setDescription(e.target.value)}
+                    />
+            </label>
+            <label>
+                <input className='host-price'
+                    type='text'
+                    placeholder="Price"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    />
+            </label>
+            <button className="host-form-button" type='submit'>
+                Create your spot!
+            </button>
+        </form>
+        </div>
+
+            )
+}
