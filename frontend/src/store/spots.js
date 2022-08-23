66b00e7131f3e4d682d191cf4c39fd2ffb4952a3
Trevor <Trevor1798@yahoo.com>
2022-08-23 15:46:25 -0400

    switching over to spots thunk

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,2 +1,38 @@
 import { csrfFetch } from "./csrf";
 
+const GETSPOTS = 'spots/getSpots'
+const CREATESPOTS = 'spots/createSpots'
+
+const allSpots = (spots) => {
+    return {
+        type: GETSPOTS,
+        spots
+    }
+}
+const createSpots = (spots) => {
+    return {
+        type: CREATESPOTS,
+        spots
+    }
+}
+
+export const getAllSpots = () => async (dispatch) => {
+    const response = await csrfFetch('api/spots')
+    if (response.ok) {
+        const spots = await response.json()
+        dispatch(getAllSpots(spots))
+        return spots
+    }
+}
+
+
+const initialState = {}
+
+const spotReducer = (state = initialState, action) => {
+    let newState = {...state}
+    switch(action.type) {
+        case GETSPOTS: {
+          const
+        }
+    }
+}
